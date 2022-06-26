0de0d5d5ac9d4947fd032e555d367f70a3d9a5a2
Nguyễn Hữu Điền <1612104@student.hcmus.edu.vn>
2022-06-26 07:08:32 +0000

    fix(stock_app): sửa lỗi do đặt sai tên file model
    lstm_model.h5 --> saved_lstm_model.h5

--- stock_app.py
+++ stock_app.py
@@ -50,7 +50,7 @@ x_train,y_train=np.array(x_train),np.array(y_train)
 
 x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 
-model=load_model("saved_model.h5")
+model=load_model("saved_lstm_model.h5")
 
 inputs=new_data[len(new_data)-len(valid)-60:].values
 inputs=inputs.reshape(-1,1)
